515764dcbb7e2e9f6c95ef5d957527b413b3ffe1
Francis Lagares <francis.lagares@gmail.com>
2021-01-14 19:18:29 +0100

    feat: create homepage component

---
+++ src/components/Discount.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Discount = (): JSX.Element => (
+  <div className="discount">
+    <div className="numbers">
+      <div>-20%</div>
+      <div>-30%</div>
+      <div>-40%</div>
+    </div>
+  </div>
+);
+
+export default Discount;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,62 @@
-import React from 'react';
+import React, { ReactElement, useState, useEffect } from 'react';
+import Discount from '../components/Discount';
 import './HomePage.css';
 
-const HomePage = (): JSX.Element => {
+interface Photos {
+  id: number;
+  photographer: string;
+  src: {
+    [key: string]: string;
+  };
+}
+
+const HomePage = (): ReactElement => {
+  const [photos, setPhotos] = useState<Photos[]>([]);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  useEffect(() => {
+    const fetchImages = async (): Promise<unknown> => {
+      const response = await fetch(
+        'https://api.pexels.com/v1/curated?per_page=9',
+        {
+          headers: {
+            Authorization:
+              '563492ad6f917000010000017655fb8ff4a14513bed976678571549d',
+          },
+        },
+      );
+      if (response.ok) {
+        const jsonResponse = await response.json();
+        setPhotos(jsonResponse.photos);
+        console.log(jsonResponse);
+      } else {
+        setErrorMessage(
+          `Failed to fetch images, error message: ${response.statusText}`,
+        );
+      }
+      return response;
+    };
+    fetchImages();
+  }, []);
+
   return (
     <div className="homepage">
-      <h1>Home Page</h1>
+      <div className="directory-menu">
+        {errorMessage && <p> {errorMessage}</p>}
+        <Discount />
+        <div className="images-container">
+          {photos.map(photo => (
+            <div key={photo.id}>
+              <img
+                src={photo.src.original}
+                width="500"
+                height="500"
+                alt={photo.photographer}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
